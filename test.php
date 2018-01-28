262a07c2ebe6cd298d5660623f286ff42923a6d8
MasterOfGit <markus.kessler.73@gmx.de>
2018-01-28 11:52:50 +0100

    Ausgeraeumt

---
+++ test.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP Testseite</title>
+</head>
+<body>
+<?php 
+echo "Lass uns mal PHP Testen !";
+phpinfo();	
+	
+?>
+
+	
+</body>
+</html>
