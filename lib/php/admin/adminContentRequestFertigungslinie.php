12337b0453f54fc54de22cbfff30a9080bab52f4
MasterOfGit <markus.kessler.73@gmx.de>
2018-02-11 16:54:31 +0100

    update

---
+++ lib/php/admin/adminContentRequestFertigungslinie.php
@@ -0,0 +1,18 @@
+<?php
+echo "PHP Datenabfrage<br>";
+$q = $_REQUEST["q"];
+echo "Anfrage : "  . $q . "<br>";
+
+?>
+
+<div class="fertigungslinie">
+  <div class="jumbotron">
+    <div class="container">
+      <h2>Fertigungslinie</h2>
+      
+	<h1>Diese Seite muss noch erstellt werden</h1>
+	
+	<input type="image" src="images/png/UnderConstructen.png">
+    </div>
+  </div>
+</div>
