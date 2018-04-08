beebce1b01cc46b2888a1417a7d93b6aaf30445c
MasterOfGit <markus.kessler.73@gmx.de>
2018-04-08 21:33:15 +0200

    update Fertigungslinie

--- lib/php/admin/Fertigungslinie/adminCRFertigungslinie.php
+++ lib/php/admin/Fertigungslinie/adminCRFertigungslinie.php
@@ -1,236 +1,110 @@
+
 <?php
 //echo "PHP Datenabfrage<br>";
 //$q = $_REQUEST[ "q" ];
 //echo "Anfrage : " . $q . "<br>";
 
-?>
+$ch1 = curl_init();
 
-<div class="Fertigungslinie">
+
+//curl_setopt($ch2, CURLOPT_URL, "http://zoomnation.selfhost.eu:8080/ProManAPI/api/adminPage/?identifier=AdminPageAbteilung");
+
+curl_setopt( $ch1, CURLOPT_URL,"http://zoomnation.selfhost.eu/jsonData/fertigungslinien/fertigungslinien.json" );
+curl_setopt( $ch1, CURLOPT_HEADER, 0 );
+curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
+$fertigungslinien = curl_exec( $ch1 );
+curl_close( $ch1 );
+
+// Testausgabe
+//echo($maschinen);
+
+// Unwandlung von json in Array	
+$jsonfertigungslinien = json_decode($fertigungslinien, TRUE);
+//print_r($fertigungslinien);
+
+//print_r($jsonfertigungslinien);
+
+echo <<<'HOME1_HEADER'
+<div class="Fetigungslinie">
 	<div class="jumbotron">
 		<div class="container">
-			<h2>Fertigungslinie</h2>
-			<br>
-			<!-- Nav tabs -->
-			<ul class="nav nav-tabs" role="tablist">
-				<li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#home1">Fertigungslinien</a>&nbsp;</li>
-				<li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#menu1">Fertigungsliniengrafik</a>
+
+			<ul class="nav nav-tabs">
+				<li class="active">
+					<a data-toggle="tab" href="#home1">Fertigungslinen bearbeiten</a>
+				</li>
+				<li>
+					<a data-toggle="tab" href="#menu1">Gertigungslinien Grafik</a>
 				</li>
 			</ul>
-
 			<!-- Tab panes -->
 			<div class="tab-content">
+				<div id="home1" class="tab-pane fade in active"><br>
 
-				<div id="home1" class="container tab-pane fade"><br>
-					<form class="form-inline" action="/action_page.php">
-						<label for="fertigung">Fertigungslinie</label>
-						<select id="fertigung">
-							<option>Fertigunglinie_1</option>
-							<option>Fertigunglinie_2</option>
-							<option>Fertigunglinie_3</option>
-						</select>
-						<label for="fertigungstype">Fertigungstyp</label>
-						<select id="fertigungstype">
-							<option>Grünfertigung</option>
-							<option>Hardfertigung</option>
-							<option>Härter</option>
-							<option>Montage</option>
-							<option>Qualitätssicherung</option>
-						</select>
-						<label for="teilenummer">Teilenummer</label>
-						<select id="teilenummer">
-							<option>Festrad_1</option>
-							<option>Festrad_2</option>
-							<option>Festrad_3</option>
-							<option>Schaldradrad_1</option>
-							<option>Schaldradrad_2</option>
-							<option>Schaldradrad_3</option>
-						</select>
-
-					</form>
 					<div class="table-responsive-sm">
 						<table class="table">
 							<thead>
 								<tr>
-									<th>Arbeitsfolge</th>
+									<th>FertigungsID</th>
+									<th>Arbeitfolge</th>
 									<th>Maschine</th>
 									<th>Technologie</th>
-									<th>Fertigungstype</th>
+									<th>Fertigungstyp</th>
 									<th>Teilenummer</th>
 									<th>Arbeitsplan</th>
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>1</td>
-									<td>
-										<select>
-											<option selected>Maschine_1</option>
-											<option>Maschine_2</option>
-											<option>Maschine_3</option>
-											<option>Maschine_4</option>
-											<option>Maschine_5</option>
-										</select>
-									</td>
-									<td>
-										Drehen
-									</td>
-									<td>
-										Grünfertigung
-									</td>
-									<td>
-										Festrad_1
-									</td>
-									<td>
-										Rohteil drehen
-									</td>
-									<td><input type="button" value="Löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-										<input type="button" value="Verschieben" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-									</td>
-								</tr>
-								<tr>
-									<td>2</td>
-									<td>
-										<select>
-											<option>Maschine_1</option>
-											<option selected>Maschine_2</option>
-											<option>Maschine_3</option>
-											<option>Maschine_4</option>
-											<option>Maschine_5</option>
-										</select>
-									</td>
-									<td>
-										Fräsen
-									</td>
-									<td>
-										Grünfertigung
-									</td>
-									<td>
-										Festrad_1
-									</td>
-									<td>
-										Verzahnung fräsen
-									</td>
-									<td><input type="button" value="Löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-										<input type="button" value="Verschieben" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-									</td>
-								</tr>
-								<tr>
-									<td>3</td>
-									<td>
-										<select>
-											<option>Maschine_1</option>
-											<option>Maschine_2</option>
-											<option selected>Maschine_3</option>
-											<option>Maschine_4</option>
-											<option>Maschine_5</option>
-										</select>
-									</td>
-									<td>
-										Entgraten
-									</td>
-									<td>
-										Grünfertigung
-									</td>
-									<td>
-										Festrad_1
-									</td>
-									<td>
-										Verzahnung entgraten
-									</td>
-									<td><input type="button" value="Löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-										<input type="button" value="Verschieben" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-									</td>
-								</tr>
-								<tr>
-									<td>4</td>
-									<td>
-										<select>
-											<option>Maschine_1</option>
-											<option>Maschine_2</option>
-											<option>Maschine_3</option>
-											<option selected>Maschine_4</option>
-											<option>Maschine_5</option>
-										</select>
-									</td>
-									<td>
-										Waschen
-									</td>
-									<td>
-										Grünfertigung
-									</td>
-									<td>
-										Festrad_1
-									</td>
-									<td>
-										none
-									</td>
-									<td><input type="button" value="Löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-										<input type="button" value="Verschieben" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-									</td>
-								</tr>
-								<tr>
-									<td>5</td>
-									<td>
-										<select>
-											<option>Maschine_1</option>
-											<option>Maschine_2</option>
-											<option>Maschine_3</option>
-											<option>Maschine_4</option>
-											<option selected>Maschine_5</option>
-										</select>
-									</td>
-									<td>
-										Messen
-									</td>
-									<td>
-										Grünfertigung
-									</td>
-									<td>
-										Festrad_1
-									</td>
-									<td>
-										none
-									</td>
-									<td><input type="button" value="Löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-										<input type="button" value="Verschieben" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-									</td>
-								</tr>
+HOME1_HEADER;
+			
+							
+						foreach($jsonfertigungslinien['fertigungslinie'] as $fertigungslinie) 
+						{	echo("<tr>");
+							
+							foreach($fertigungslinie['arbeitsfolgen'] as $arbeitsfolge) 
+								{ 	echo("<td>{$fertigungslinie['fertigungslinieID']}</td>");
+										echo("<td>{$arbeitsfolge['arbeitsfolgeID']}</td>"); 
+										echo("<td>{$arbeitsfolge['maschineID']}</td>");
+										echo("<td>{$arbeitsfolge['technologie']}</td>");
+										echo("<td>{$arbeitsfolge['fertigungstyp']}</td>");
+										echo("<td>{$arbeitsfolge['bauteilID']}</td>");
+										echo("<td>{$arbeitsfolge['arbeitplan']}</td>");
+
+										echo("<td><input type='button' value='löschen'  onclick='testbuttonaction();'></td>");
+										echo("<td><input type='button' value='verschieben'  onclick='testbuttonaction();'></td>");
+								echo("</tr>");
+								};
+						 
+						};
+echo("<td><input type='button' value='Neue Arbeitfolge'  onclick='testbuttonaction();'></td>");
+echo("<td><input type='button' value='Speichern'  onclick='testbuttonaction();'></td>");
+echo("<td><input type='button' value='Reset'  onclick='testbuttonaction();'></td>");
+echo("<td><input type='button' value='Neu Fertigungslinie'  onclick='testbuttonaction();'></td>");
+echo("<td><input type='button' value='Fertigunglinie löschen'  onclick='testbuttonaction();'></td>");
+
+
+echo <<<'HOME1_FOOTER'
 							</tbody>
 						</table>
-						<div>
-							<input type="button" value="Neu Arbeitsfolge hinzufügen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-							<input type="button" value="Alles Speicher" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-							<input type="button" value="Reset" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-						</div>
-						<br>
-						<div>
-							<input type="button" value="Neu Fertigungslinie anlegen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-							<input type="button" value="Fertigungslinie löschen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-							</divv>
-							<br>
-							<div>
-
-							</div>
-						</div>
 					</div>
 				</div>
-
-
-				<div id="menu1" class="container tab-pane fade"><br>
+HOME1_FOOTER;
+						 
+echo("test");
+						 
+echo <<<'MENU1_HEADER'
+<div id="menu1" class="container tab-pane fade"><br>
 					<form class="form-inline" action="/action_page.php">
-
-
 					</form>
 					<canvas id="myCanvas" width="400" height="400" style="border:1px solid #d3d3d3;">
 						Your browser does not support the HTML5 canvas tag.</canvas>
-				
-
-
-
 					<input type="button" value="Grafik generierren" onclick="createCanvas()">
 
-				</div>
 			</div>
 		</div>
+      </div>	
 	</div>
 </div>
+
+MENU1_HEADER;
+?>
--- lib/php/admin/Maschine/adminCRMaschinen.php
+++ lib/php/admin/Maschine/adminCRMaschinen.php
@@ -3,222 +3,170 @@
 //$q = $_REQUEST["q"];
 //echo "Anfrage : "  . $q . "<br>";
 
-//post request
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['savedb']) and isset($_POST['inventarID']))
-{
-      SendFunction();
-}
-function SendFunction()
-  {
-      $data = array("Inventarnummer"=>$_POST['inventarID'],"Technologie"=>$_POST['technologie'],"Hersteller"=>$_POST['hersteller'],"Status"=>$_POST['bearbeitungsstand'],"Version"=>$_POST['maschienVersion'],"Zeichnungsnummer"=>$_POST['maschineZeichnungsnummer'],"Standort"=>$_POST['standort'],"Anschaffungsdatum"=>$_POST['anschaffungsdatum'],"Garantie"=>$_POST['garantieBis']);                                                                    
-      $data_string = json_encode($data);                                                                                   
-                                                               
-      $ch = curl_init();  
-      curl_setopt($ch,CURLOPT_URL,"http://zoomnation.selfhost.eu:8080/ProManAPI/api/maschine");                                                                    
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-          'Content-Type: application/json',                                                                                
-          'Content-Length: ' . strlen($data_string))                                                                       
-      );  
-      curl_exec($ch);
-  }
+
+// Data load
+
+$ch1 = curl_init();
+$ch2 = curl_init();
+$ch3 = curl_init();
 
 
-//get request
+//curl_setopt($ch1, CURLOPT_URL, "http://zoomnation.selfhost.eu:8080/ProManAPI/api/adminPage/?identifier=AdminPageUser");
 
-$cSession = curl_init(); 
-curl_setopt($cSession,CURLOPT_URL,"http://zoomnation.selfhost.eu:8080/ProManAPI/api/adminPage/?identifier=AdminPageMaschine");
-curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($cSession,CURLOPT_HEADER, false); 
-$result=curl_exec($cSession);
+curl_setopt($ch1, CURLOPT_URL, "http://zoomnation.selfhost.eu/jsonData/maschinen/maschinen.json");
 
-curl_close($cSession);
+curl_setopt($ch1, CURLOPT_HEADER, 0);
+curl_setopt($ch1,CURLOPT_RETURNTRANSFER,true);
+$maschinen=curl_exec($ch1);
+curl_close($ch1);
 
-$json = json_decode($result, TRUE);
+//curl_setopt($ch2, CURLOPT_URL, "http://zoomnation.selfhost.eu:8080/ProManAPI/api/adminPage/?identifier=AdminPageAbteilung");
+
+curl_setopt($ch2, CURLOPT_URL, "http://zoomnation.selfhost.eu/jsonData/maschinen/maschinenVerwendung.json");
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2,CURLOPT_RETURNTRANSFER,true);
+$maschinenVerwendung=curl_exec($ch2);
+curl_close($ch2);
+
+curl_setopt($ch3, CURLOPT_URL, "http://zoomnation.selfhost.eu/jsonData/bauteile/bauteile.json");
+curl_setopt($ch3, CURLOPT_HEADER, 0);
+curl_setopt($ch3,CURLOPT_RETURNTRANSFER,true);
+$bauteile=curl_exec($ch3);
+curl_close($ch3);
 
 
+// Testausgabe
+//echo($maschinen);
+
+// Unwandlung von json in Array	
+$jsonmaschinen = json_decode($maschinen, TRUE);
+$jsonmaschinenVerwendung = json_decode($maschinenVerwendung, TRUE);
+echo($jsonmaschinen);
 echo <<<'HOME1_HEADER'
+<div class="User">
+  	<div class="jumbotron">
+    	<div class="container">
 
-<div class="Maschinen">
-  <div class="jumbotron">
-    <div class="container">
-      <h2>Maschine</h2>
-      <br>
-      <!-- Nav tabs -->
-      <ul class="nav nav-tabs" role="tablist">
-        <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#home1">Maschine bearbeiten</a>&nbsp;</li>
-        <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#menu1">Maschinenverwendung</a></li>
-      </ul>
-      
-      <!-- Tab panes -->
-      <div class="tab-content">
-        <div id="home1" class="container tab-pane fade"><br>
-           <form class="form-inline" action="/action_page.php">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-success" type="submit">Search</button>
-          </form>
-          <div class="table-responsive-sm">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Inventarnummer</th>
-                  <th>Hersteller</th>
-                  <th>Technologie</th>
-                  <th>Standort</th>
-                  <th>Status</th>
-				  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-			  
-			  	<tr>
-                  <td>Maschine_1</td>
-                  <td>Scherre</td>
-                  <td>Drehen</td>
-                  <td>Werk 1 Halle 5 Feld D6</td>
-                  <td>Prouktiv</td>
-				  <td><input type="button" value="Maschine bearbeiten"  onclick="loadDoc('lib/php/admin/Maschine/adminCRMaschineBear.php?q=1111',myFunction1)"></td>
-                </tr>
-				
-				<tr>
-                  <td>Maschin_11232</td>
-                  <td>Weisser</td>
-                  <td>Schleifen</td>
-                  <td>Werk 16 Halle 1 Feld A7</td>
-                  <td>ImAufbau</td>
-				  <td><input type="button" value="Maschine bearbeiten"  onclick="loadDoc('lib/php/admin/Maschine/adminCRMaschineBear.php?q=1111',myFunction1)"></td>
-                </tr> 
-				<tr>
-                  <td>Maschin_888</td>
-                  <td>Weisser</td>
-                  <td>Schleifen</td>
-                  <td>Werk 16 Halle 3 Feld E1</td>
-                  <td>Umbau</td>
-				  <td><input type="button" value="Maschine bearbeiten"  onclick="loadDoc('lib/php/admin/Maschine/adminCRMaschineBear.php?q=1111',myFunction1)"></td>
-                </tr> 
-			  
+			<ul class="nav nav-tabs">
+			  <li class="active">
+			  	<a data-toggle="tab" href="#home1">Maschine bearbeiten</a>
+			  </li>
+			  <li>
+			    <a data-toggle="tab" href="#menu1">Maschinenverwendung</a>
+			  </li>
+			 
+
+		   
+			 <!-- Tab panes -->
+			 <div class="tab-content">
+					<div id="home1" class="tab-pane fade in active"><br>
+
+					  <div class="table-responsive-sm">
+						<table class="table">
+						  <thead>
+							<tr>
+							  <th>MaschineID</th>
+							  <th>Inventarnummer</th>
+							  <th>Hersteller</th>
+							  <th>Technologie</th>
+							  <th>Standort</th>
+							  <th>Abteilung</th>
+							  <th>Status</th>
+							</tr>
+						  </thead>
+						  <tbody>  
 HOME1_HEADER;
-<<<<<<< HEAD
-   /*           foreach($json['Maschinen'] as $item)
+
+				foreach($jsonmaschinen['maschinen'] as $maschine)
 				{
 				 
-				echo("<tr>");
-				echo("<td>{$item['Zeichnungsnummer']}</td>");
-				echo("<td>{$item['Hersteller']}</td>");
-	  			echo("<td>{$item['Technologie']}</td>");
-				echo("<td>{$item['Standort'] }</td>");
-				echo("<td>{$item['Status']}</td>");
-	  			echo("<td><input type='button' value='Maschdaten bearbeiten' onclick='loadDoc('lib/php/admin/adminContentRequestMaschineBear.php?q=1111,myFunction1)'></td>");
+					echo("<tr>");
+					echo("<td>{$maschine['maschinenID']}</td>");
+					echo("<td>{$maschine['maschinenInventarNummer']}</td>");
+					echo("<td>{$maschine['hersteller']}</td>");
+					echo("<td>{$maschine['technologie']}</td>");
+					echo("<td>{$maschine['standort'] }</td>");
+					echo("<td>{$maschine['abteilung']}</td>");
+					echo("<td>{$maschine['status']}</td>");
+					echo("<td><input type='button' value='Bearbeiten'  onclick='testbuttonaction({maschine['maschinenID']});'></td>");
 				echo("</tr>");
 				};
-	*/				
+				echo("<td><input type='button' value='Neues Maschine anlegen'  onclick='testbuttonaction();'></td>");
 
-=======
-              foreach($json['Maschinen'] as $item)
-              {
-              
-              echo("<tr>");
-              echo("<td>{$item['Zeichnungsnummer']}</td>");
-              echo("<td>{$item['Hersteller']}</td>");
-              echo("<td>{$item['Technologie']}</td>");
-              echo("<td>{$item['Standort'] }</td>");
-              echo("<td>{$item['Status']}</td>");
-              echo("<td><input type='button' value='Maschdaten bearbeiten' onclick='loadDoc('lib/php/admin/adminContentRequestMaschineBear.php?q=1111,myFunction1)'></td>");
-              echo("</tr>");
-              };
-					
-                  
-               
-                          
->>>>>>> origin/master
-echo <<<'HOME1_FOOTER'
-			</tbody>
 
-			</table>
-            <br>
-			  <input type="button" value="Neue Maschine"  onclick="loadDoc('lib/php/admin/Maschine/adminCRMaschineBear.php?q=1111',myFunction1)">
-          </div>
-        </div>
+echo <<<HOME1_FOOTER
+						 </tbody>
+            		  </table>
+          		   </div>
+        		</div>
+		
 HOME1_FOOTER;
 
-echo <<<'MENU1_FOOTER'
+			  
 
-        <div id="menu1" class="container tab-pane active"><br>
-          <h3>Maschinen</h3>
-          <form class="form-inline" action="/action_page.php">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-success" type="submit">Search</button>
-          </form>
-          <div class="table-responsive-sm">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Inventarnummer</th>
-                  <th>Hersteller</th>
-                  <th>Technologie</th>
-                  <th>Standort</th>
-                  <th>Status</th>
-                  <th>Abteilung</th>
-                  <th>Fertigung</th>
-                  <th>Fertigungslinie</th>
-				  <th>Teilename</th>	
-                  <th>Arbeitsfolge</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Maschine_10</td>
-                  <td>Scherer</td>
-                  <td>Fräsen</td>
-                  <td>Werk 12 Halle 10 Feld C3 HG</td>
-                  <td>Produktiv</td>
-                  <td>Abteilung_1</td>
-                  <td>Grünfertigung</td>
-                  <td>Fertigungslinie_7</td>
-				  <td>Festrad_5</td>	
-                  <td>5</td>
-                  <td><input type="button" value="Verw. Bearbeiten"  onclick="loadDoc('lib/php/admin/Maschine/adminCRMaschineVerwBear.php?q=1111',myFunction1)"></td>
-                </tr>
-                <tr>
-                  <td>Maschine_300</td>
-                  <td>Weisser</td>
-                  <td>Schleifen</td>
-                  <td>Werk 12 Halle 1 Feld A5 EG</td>
-                  <td>Umbau</td>
-                  <td> Abteilung_7</td>
-                  <td>Hardfertigung</td>
-                  <td>Fertigungslinie_4</td>
-					<td>Festrad_7</td>
+echo <<<'MENU1_HEADER'
+			     <div id="menu1" class="tab-pane fade">           
+				  <div class="table-responsive-sm">
+					<table class="table">
+					  <thead>
+						<tr>
+						 <th>MaschinenID</th>
+						  <th>Inventarnummer</th>
+						  <th>Hersteller</th>
+						  <th>Technologie</th>
+						  <th>Status</th>
+						  <th>Abteilung</th>
+						  <th>Fertigungslinie</th>
+						  <th>Arbeitsfolge</th>
+						</tr>
+					  </thead>
+					  <tbody>
+MENU1_HEADER;
+
+				foreach($jsonmaschinenVerwendung['machinenVerwendung'] as $maschinenverwendung)
+				{	
 					
-                  <td>4</td>
-                  <td><input type="button" value="Verw. Bearbeiten" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)"></td>
 					
-                </tr>
-				  
-				   <tr>
-                  <td>Maschine_1000</td>
-                  <td>Weisser</td>
-                  <td>Schleifen</td>
-                  <td></td>
-                  <td>Neu</td>
-                  <td></td>
-                  <td>Hardfertigung</td>
-				  <td></td>	   
-                  <td>Festrad_4</td>
-                  <td> </td>
-                  <td><input type="button" value="Verw. Bearbeiten" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)"></td>
-                </tr>
-              </tbody>
-            </table>
-			<input type="button" value="Neue Maschinen Verwendung anlegen" onclick="loadDoc('lib/php/admin/adminContentRequestMaschineVerwBear.php?q=1111',myFunction1)">
-          </div>
-        </div>
-      </div>
-    </div>
+				 	foreach($jsonmaschinen['maschinen'] as $maschinen)
+					{
+						//echo($maschinen['maschinenID']);
+						//echo("<br>");
+						
+						if($maschinen['maschinenID'] == $maschinenverwendung['maschinenID'] )
+						{
+						echo("<tr>");
+						echo("<td>{$maschinen['maschinenID']}</td>");
+						echo("<td>{$maschinen['maschinenInventarNummer']}</td>");
+						echo("<td>{$maschinen['hersteller']}</td>");
+						echo("<td>{$maschinen['technologie']}</td>");
+						echo("<td>{$maschinen['status'] }</td>");
+						echo("<td>{$maschinen['abteilung']}</td>");
+						echo("<td>{$maschinenverwendung['fertigungID']}</td>");
+						echo("<td>{$maschinenverwendung['arbeitsfolge']}</td>");
+						echo("<td><input type='button' value='Bearbeiten'  
+						onclick='testbuttonaction({$maschinen['maschinenID']});'></td>");
+						};
+					};
+				};
+						
+						echo("</tr>");
+				
+				echo("<td><input type='button' value='Neues Maschine anlegen'  onclick='testbuttonaction();'></td>");
+
+
+echo <<<'MENU1_FOOTER'
+							</tbody>
+							</table>
+							
+						  </div>
+						</div>
+			</div>
+	 </div>
+   </div>
   </div>
 </div>
 MENU1_FOOTER;
 
 ?>
+
+
