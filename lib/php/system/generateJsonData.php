198b0af954ab55b20ebe52ac9abc1bb235c754ea
MasterOfGit <markus.kessler.73@gmx.de>
2018-04-12 18:35:35 +0200

    update
    generatejsondata.pho und dbbuttonaction.js
    gerüst

---
+++ lib/php/system/generateJsonData.php
@@ -0,0 +1,94 @@
+<?php
+
+//$url = "fertigungslinien.json";
+////$url2 = "save.json";
+//
+//$file = file_get_contents($url, FILE_USE_INCLUDE_PATH);
+//
+//$jsoninput = json_decode($file, TRUE);
+//
+//$a = $jsoninput['fertigungslinie'];
+//
+//foreach( $a as &$b)
+//{	
+//	if($b['fertigungslinieID'] == 1)
+//	{
+//		$b['fertigunglinenname'] = "Fertigung_1";
+//	}
+// 	
+//};
+//$b = json_encode($a);
+//
+//file_put_contents($url, $b);
+
+// json Dateien laden
+
+function loaduserLogin(){
+	
+};
+
+function loaduserData(){
+	
+};
+function loaduserAnfragea(){
+	
+};
+function loadwartungen(){
+	
+};
+
+function loadaudits(){
+	
+};
+
+function loadproduktionsplanung(){
+	
+};
+
+function loadlagerbestend(){
+	
+};
+function loadmaschinenverwendung(){
+	
+};
+
+function loadmaschinen(){
+	
+};
+
+function loadinstandhaltung(){
+	
+};
+
+function loadfertigungslinie(){
+	
+};
+
+function loadfertigung(){
+	
+};
+
+function loadbauteiverwendung(){
+	
+};
+
+function loadbauteil(){
+	
+};
+
+function loadfertigunggststusdeteils()){
+	
+};
+
+function loadabteilungfertigungsstatus(){
+	
+};
+
+function loaduserData(){
+	
+};
+
+function loadabteilung(){
+	
+};
+?>
