c55a219d5b423d8229e0e2928b211ac1849b6dcc
MasterOfGit <markus.kessler.73@gmx.de>
2018-04-16 23:42:20 +0200

    update json datei erstellen
    für bauteile

---
+++ lib/php/admin/Bauteil/saveBauteilJsonDatei.php
@@ -0,0 +1,19 @@
+<?php
+
+$ch1 = curl_init();
+
+curl_setopt( $ch1, CURLOPT_URL,"http://zoomnation.selfhost.eu/jsonData/bauteile/bauteile.json" );
+//curl_setopt( $ch1, CURLOPT_URL,"http://zoomnation.selfhost.eu:8080/ProManAPI/API/Audits" );
+curl_setopt( $ch1, CURLOPT_HEADER, 0 );
+curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
+$audits = curl_exec( $ch1 );
+curl_close( $ch1 );
+
+//echo($audits);
+
+//$dateiname = 'C:/xampp/htdocs/proman/jsonData/sonderaufgaben/auditstest.json';
+$dateiname = '../../../../jsonData/bauteile/bauteile.json';
+
+file_put_contents($dateiname, $audits);
+
+?>
